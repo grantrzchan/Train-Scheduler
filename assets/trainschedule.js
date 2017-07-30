2d2bc1de13cf3f0553780f06b60b3920f33a21dd
Grant Chan <grant.r.chan@gmail.com>
2017-07-30 18:19:28 -0500

    2nd update

--- assets/trainschedule.js
+++ assets/trainschedule.js
@@ -12,41 +12,57 @@ $("#addTrain").on("click", function(event) {
   var Destination = $("#destination").val().trim();
   var FirstTrain = $("#firstTrain").val().trim();
   var Frequency = $("#frequency").val().trim();
+  var Mins_Away = null;
+  var MinutesAway = null;
+  var TimeNextArrival = null;
+  var TimeNow = moment();
 
   //local object for new train data
   var newTrain = {
     name: TrainName,
     dest: Destination,
     firstChoo: FirstTrain,
-    freq: Frequency
-//  dateAdded: firebase.database.ServerValue.TIMESTAMP
+    freq: Frequency,
+ dateAdded: firebase.database.ServerValue.TIMESTAMP
   }
 
-//dataRef.ref()
+  dataRef.ref()
 
   //push new train object to firebase
   database.ref().push(newTrain);
 
-  $('#trainName').val('');
-  $('#destination').val('');
-  $('#firstTrain').val('');
-  $('#frequency').val('');
+  $('#trainName').val("");
+  $('#destination').val("");
+  $('#firstTrain').val("");
+  $('#frequency').val("");
 
   // return false;
 });
 
 
-database.ref().on('child_added', function(childSnapshot) {
+database.ref().on("child_added", function(childSnapshot) {
 
-  var TrainName = childSnapshot.val().name;
-  var Destination = childSnapshot.val().dest;
-  var FirstTrain = childSnapshot.val().firstChoo;
-  var Frequency = childSnapshot.val().freq;
-  // var Mthworkmult = moment(startDate). 
-$('.tableTrain').append("<tr><td>" + TrainName + "</td><td>" + Destination + "</td><td>" + FirstTrain + "</td><td>" + Frequency + "</td><td>" + "MinutesAway" + "</td></tr>");
+  TrainName = childSnapshot.val().name;
+  Destination = childSnapshot.val().dest;
+  FirstTrain = childSnapshot.val().firstChoo;
+  Frequency = childSnapshot.val().freq;
+  MinutesAway = ArrivingIn();
+  TimeNextArrival = moment().add(MinutesAway, "minutes").format("HH:mm");
+  console.log(moment(TimeNextArrival).format("HH:mm"));
 
+$('.tableTrain').append("<tr><td>" + TrainName + "</td><td>" + Destination + "</td><td>" + Frequency + "</td><td>" + TimeNextArrival + "</td><td>" + MinutesAway + "</td></tr>");
 
 });
 
+//Function to determine Minutes Away, stored locally
+function ArrivingIn(){
+  //Determine first train service time, train service started 6 months ago
+var FirstService = moment(FirstTrain,"hh:mm").subtract(6,"months");
+console.log(moment().format("hh:mm"));
+var t_remainder = FirstService%Frequency;
+console.log(t_remainder);
+Mins_Away = Frequency - t_remainder;
+return  Mins_Away
+}
 
 
